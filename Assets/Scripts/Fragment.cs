84031163a37e76286f9cfe1687bea05374bd8c65
Michael Frank <mfrankpb@icloud.com>
2022-07-26 17:53:41 +0200

    Fixes uvs and normals in mesh generation.

--- Assets/Scripts/Fragment.cs
+++ Assets/Scripts/Fragment.cs
@@ -17,8 +17,12 @@ public class Fragment : MonoBehaviour
         
     }
 
-    public void Generate(Polygon Data, float depth, Material FrontMat, Material SideMat)
+    public void Generate(Polygon Data, float depth, Material FrontMat, Material SideMat, Vector3 objectSize)
 	{
+		Vector2 uvScale = new Vector2(1.0f / objectSize.x, -1.0f / objectSize.z);
+		Vector3 depthVec = new Vector3(0, -depth, 0);
+		Vector3 halfDepthVec = new Vector3(0, depth * 0.5f, 0);
+
 		MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
 		MeshFilter meshFilter = GetComponent<MeshFilter>();
 		MeshCollider meshCollider = GetComponent<MeshCollider>();
@@ -44,9 +48,9 @@ public class Fragment : MonoBehaviour
 
 		for (int i = 0; i < EdgeCount; ++i)
 		{	
-			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc;
-			normals[normalIndex++] = -Vector3.forward;
-			uv[uvIndex++] = Data.Edges[i].Point2.Uv / 10.0f;
+			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc + halfDepthVec;
+			normals[normalIndex++] = Vector3.up;
+			uv[uvIndex++] = (Data.Edges[i].Point2.Uv * uvScale) + new Vector2(0.5f,0.5f);
 		}
 
 		for (int i = 0; i < EdgeCount - 2; ++i)
@@ -57,12 +61,12 @@ public class Fragment : MonoBehaviour
 		}
 
 		// Back
-		Vector3 depthVec = new Vector3(0, -depth, 0);
+		
 		for (int i = 0; i < EdgeCount; ++i)
 		{
-			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc + depthVec;
-			normals[normalIndex++] = Vector3.forward;
-			uv[uvIndex++] = Data.Edges[i].Point2.Uv / 10.0f;
+			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc - halfDepthVec;
+			normals[normalIndex++] = -Vector3.up;
+			uv[uvIndex++] = (Data.Edges[i].Point2.Uv * uvScale) + new Vector2(0.5f, 0.5f);
 		}
 
 		for (int i = 0; i < EdgeCount - 2; ++i)
@@ -77,8 +81,8 @@ public class Fragment : MonoBehaviour
 		// Sides
 		for (int i = 0; i < EdgeCount; ++i)
 		{
-			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc;
-			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc + depthVec;
+			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc + halfDepthVec;
+			vertices[vertexIndex++] = Data.Edges[i].Point2.Loc - Data.Centroid.Loc - halfDepthVec;
 			normals[normalIndex++] = Vector3.forward;
 			normals[normalIndex++] = Vector3.forward;
 
@@ -109,7 +113,7 @@ public class Fragment : MonoBehaviour
 
 
 		// Debug
-		Vector3 testoffset = new Vector3(0, Random.Range(-0.05f, 0.05f), 0);
+		Vector3 testoffset = new Vector3(0, Random.Range(-0.22f, 0.22f), 0);
 		Color randomColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
 		for (int i = 1; i < EdgeCount; ++i)
 		{
@@ -124,16 +128,16 @@ public class Fragment : MonoBehaviour
 		mesh.uv = uv;
 		mesh.normals = normals;
 		mesh.triangles = triangles;
-
+		
 		meshFilter.mesh = mesh;
-
+		
 		mesh.subMeshCount = 2;
 		mesh.SetSubMesh(0, new SubMeshDescriptor(0, FirstSubmeshLength));
 		mesh.SetSubMesh(1, new SubMeshDescriptor(FirstSubmeshLength, triangleIndex - FirstSubmeshLength));
-
+		
 		Material[] Mats = { FrontMat, SideMat };
 		meshRenderer.materials = Mats;
-
+		
 		meshCollider.sharedMesh = mesh;
 	}
 }
